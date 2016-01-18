5b98cdb34ed5574f1f0259d61bc685c9db861e1c
DraperStudio <brideloper@gmail.com>
2016-01-18 06:02:55 -0500

    Applied fixes from StyleCI

--- src/Repositories/Criteria/WhereBetween.php
+++ src/Repositories/Criteria/WhereBetween.php
@@ -1,28 +1,21 @@
 <?php
 
-
-
 namespace DraperStudio\Database\Repositories\Criteria;
 
 use DraperStudio\Database\Contracts\Repositories\Repository;
 use Illuminate\Database\Eloquent\Model;
 
-
 class WhereBetween extends Criterion
 {
-
     private $column;
 
-
     private $values;
-
 
     public function __construct($column, array $values)
     {
         $this->column = $column;
         $this->values = $values;
     }
-
 
     public function apply(Model $model, Repository $repository)
     {
