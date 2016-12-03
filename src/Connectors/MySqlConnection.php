95aa1c4cb9bb0e84ed04072f87e6f993e59bc085
Brian Faust <hello@brianfaust.de>
2016-12-03 06:39:26 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Connectors/MySqlConnection.php
+++ src/Connectors/MySqlConnection.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of Laravel Database.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Database\Connectors;
 
 use BrianFaust\Database\Schema\MySqlBuilder;
--- src/Schema/MySqlBuilder.php
+++ src/Schema/MySqlBuilder.php
@@ -1,9 +1,18 @@
 <?php
 
+/*
+ * This file is part of Laravel Database.
+ *
+ * (c) Brian Faust <hello@brianfaust.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace BrianFaust\Database\Schema;
 
-use Illuminate\Database\Schema\MySqlBuilder as BaseBuilder;
 use BrianFaust\Database\Traits\Schema\BuilderTrait;
+use Illuminate\Database\Schema\MySqlBuilder as BaseBuilder;
 
 class MySqlBuilder extends BaseBuilder
 {
